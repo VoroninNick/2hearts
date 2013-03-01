494213ef2b82e00181775ed151aff85b7bcd63de
Vik Ewoods <vikewoods@Viks-Mac-mini.local>
2013-03-01 17:54:53 +0200

    Upload new watermark model

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,4 @@
+require 'paperclip_processors/watermark'
 class Asset < ActiveRecord::Base
   attr_accessible :assetable_id, :assetable_type, :attachment
 
@@ -8,8 +9,8 @@ class Asset < ActiveRecord::Base
 
   # Paperclip image attachments
   has_attached_file :attachment, :processors => [:watermark], :styles => { :thumb => '150x150>',
-                                                                           :main => { :geometry => "1440x900#", :watermark_path => Rails.root + "/images/logo.png", :position => "East" },
-                                                                           :medium => { :geometry => "800x600#", :watermark_path => Rails.root + "/images/logo.png", :position => "East" }
+                                                                           :main => { :geometry => "1440x900#", :watermark_path => "#{Rails.root}/public/images/logo.png", :position => "East" },
+                                                                           :medium => { :geometry => "800x600#", :watermark_path => "#{Rails.root}/public/images/logo.png", :position => "East" }
   },
                     :url    => '/assets/attachment/:id/:style/:basename.:extension',
                     :path   => ':rails_root/public/assets/attachment/:id/:style/:basename.:extension'
