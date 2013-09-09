964df6ff94a0f0e99d3c4a92a0ccb38f79fdc638
Ubuntu Server <root@voroninstudio.eu>
2013-09-09 20:05:30 +0300

    Savings

--- app/assets/javascripts/core_scripts.js
+++ app/assets/javascripts/core_scripts.js
@@ -8,30 +8,61 @@ $(document).ready(function() {
     return false;
 });
 
-$("#show-menu-13").click(function(){
-	$("#show-menu-13 ul").slideToggle("slow", function(){});
-	$(this).toggleClass("sub-active");
+$("#show-menu-13").click(function(event){
+	
+  if(window.location.pathname == "/services/spetsefekti/zvuk-svitlo" || window.location.pathname == "/services/spetsefekti/pirotehnika" || window.location.pathname == "/services/spetsefekti/novaya-sab-kategoriya" || window.location.pathname == "/services/spetsefekti/Low-fog"){
+    //event.preventDefault();
+  }else{
+    $("#show-menu-13 ul").slideToggle("slow", function(){});
+  	$(this).toggleClass("sub-active");
 
-  $("#show-13").each(function (i) {
-    if($(this).text() != "-"){
-      $("#show-13").text("-");
-    }else{
-      $("#show-13").text("+");
-    }
-  });
+    $("#show-13").each(function (i) {
+      if($(this).text() != "-"){
+        $("#show-13").text("-");
+      }else{
+        $("#show-13").text("+");
+      }
+    });
+  }
+
 });
 
-$("#show-menu-16").click(function(){
-	$("#show-menu-16 ul").slideToggle("slow", function(){});
-	$(this).toggleClass("sub-active");
+$("#show-menu-16").click(function(event){
+  
+  if(window.location.pathname == "/services/shou-programi/shou-baleti" || window.location.pathname == "/services/shou-programi/zirki-estradi" || window.location.pathname == "/services/shou-programi/artisti"){
+    //event.preventDefault();
+  }else{
+    $("#show-menu-16 ul").slideToggle("slow", function(){});
+    $(this).toggleClass("sub-active");
 
-  $("#show-16").each(function (i) {
-    if($(this).text() != "-"){
-      $("#show-16").text("-");
-    }else{
-      $("#show-16").text("+");
-    }
-  });
+    $("#show-16").each(function (i) {
+      if($(this).text() != "-"){
+        $("#show-16").text("-");
+      }else{
+        $("#show-16").text("+");
+      }
+    });
+  }
+
+});
+
+$("#show-menu-17").click(function(event){
+  
+  if(window.location.pathname == "/services/eksklyuziv/zaproshennya" ){
+    //event.preventDefault();
+  }else{
+    $("#show-menu-17 ul").slideToggle("slow", function(){});
+    $(this).toggleClass("sub-active");
+
+    $("#show-17").each(function (i) {
+      if($(this).text() != "-"){
+        $("#show-17").text("-");
+      }else{
+        $("#show-17").text("+");
+      }
+    });
+  }
+
 });
 
 });
